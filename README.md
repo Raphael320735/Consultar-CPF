2208cc041d459f7dea58acb85a89a365b0fcaa09
Gustavo Benevenuto <gustavo.benesilva@gmail.com>
2019-05-23 22:17:50 -0300

    Primeira Versao

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Consultar-CPF
 Programa em java para consultar CPF's
+
+## Assuntos Abordados no Desenvolvimento do Projeto:
+
+- Lógica de Programação
+- Tratamento de erro
---
+++ src/consultar/cpf/ConsultarCPF.java
@@ -0,0 +1,9 @@
+package consultar.cpf;
+public class ConsultarCPF {
+    public static void main(String[] args) {
+        frmTelaConsulta frmConsulta = new frmTelaConsulta();
+        frmConsulta.show();
+        
+    }
+    
+}
